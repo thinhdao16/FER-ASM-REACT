e3f4e6253a227da3f22ea0a6f9b7daa776654341
thinh <daothinh1105@gmail.com>
2022-07-19 01:26:43 +0700

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,51 +1,68 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
+import Contact from './ContactComponent';
+//import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import Home from './HomeComponent';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
+
 class Main extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES,
-            selectedDish: null
-        };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
+    };
+  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //       dishes: DISHES,
+  //       selectedDish: null
+  //   };
+  // }
+
+  render() {
+
+    const HomePage = () => {
+      return(
+          <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
+      );
     }
+    // const HomePage = () => {
+    //   return(
+    //       <Home 
+    //       />
+    //   );
+    // }
 
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId });
-    }
-
-    render() {
-        const HomePage = () => {
-            return (
-                <Home
-                />
-            );
-        }
-
-        return (
-            <div>
-                <Header />
-                <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-                <Footer />
-            </div>
-        );
+    return (
+      <div>
+        <Header />
         <Switch>
-            <Route path='/home' component={HomePage} />
-            <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-            <Redirect to="/home" />
+          <Route path='/home' component={HomePage} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path='/contactus' component={Contact} />
+          <Redirect to="/home" />
         </Switch>
+        <Footer />
+      </div>
+    );
 
-
-    }
+  }
 }
 
 export default Main;
-
